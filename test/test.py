0749bd5df4f90bc4d979cac5ebf808360c1f9a58
codml <kimtawann@naver.com>
2023-12-10 01:43:29 +0900

    auto

--- test/test.py
+++ test/test.py
@@ -1,9 +1,13 @@
 import random as rd
 
-f = open("test.txt", 'w')
-for i in range(1, 11):
+f = open("graph_L.txt", 'w')
+f.write('L\n')
+f.write('15\n')
+for i in range(1, 16):
 	f.write('%d\n' % i)
-	for j in range(i + 1, 11):
-		rand = rd.randint(1, 100)
-		f.write('%d %d\n' % (j, rand))
+	for j in range(i + 1, 16):
+		randnum = rd.randint(0, 9)
+		if randnum >= 5:
+			rand = rd.randint(-10, 100)
+			f.write('%d %d\n' % (j, rand))
 f.close()
